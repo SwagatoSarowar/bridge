3f5fef74c96b793ffe86c1f8a9ddda147df0ebf9
SwagatoSarowar <swagotosarowar1@gmail.com>
2024-02-07 16:49:32 +0600

    removed tests

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,6 @@ import PostInputField from "../components/PostInputField";
 import CreatePostModal from "../components/CreatePostModal";
 import { getDatabase, onValue, ref } from "firebase/database";
 import Posts from "../components/Posts";
-import { InlineShareButtons } from "sharethis-reactjs";
 
 export default function Home() {
   const auth = getAuth();
@@ -92,41 +91,6 @@ export default function Home() {
                     friendsIdList={friendsIdList}
                   />
                 ))}
-              </div>
-              <div>
-                <InlineShareButtons
-                  config={{
-                    alignment: "center", // alignment of buttons (left, center, right)
-                    color: "social", // set the color of buttons (social, white)
-                    enabled: true, // show/hide buttons (true, false)
-                    font_size: 16, // font size for the buttons
-                    labels: "cta", // button labels (cta, counts, null)
-                    language: "en", // which language to use (see LANGUAGES)
-                    networks: [
-                      // which networks to include (see SHARING NETWORKS)
-                      "whatsapp",
-                      "linkedin",
-                      "messenger",
-                      "facebook",
-                      "twitter",
-                    ],
-                    padding: 12, // padding within buttons (INTEGER)
-                    radius: 4, // the corner radius on each button (INTEGER)
-                    show_total: true,
-                    size: 40, // the size of each button (INTEGER)
-
-                    // OPTIONAL PARAMETERS
-
-                    min_count: 10, // (threshold for total share count to be displayed)
-                    // url: "https://www.sharethis.com", // (defaults to current url)
-                    image: "https://bit.ly/2CMhCMC", // (defaults to og:image or twitter:image)
-                    description: "custom text", // (defaults to og:description or twitter:description)
-                    title: "custom title", // (defaults to og:title or twitter:title)
-                    message: "custom email text", // (only for email sharing)
-                    subject: "custom email subject", // (only for email sharing)
-                    username: "custom twitter handle", // (only for twitter sharing)
-                  }}
-                />
               </div>
             </MainContainer>
           </div>
